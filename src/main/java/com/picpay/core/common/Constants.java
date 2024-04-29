790a943729396bb24deb5d2155606fd37cd2007a
neemiasgabriel <neemias.arruda@picpay.com>
2024-04-29 12:16:30 -0300

    Basic validations

--- src/main/java/com/picpay/core/common/Constants.java
+++ src/main/java/com/picpay/core/common/Constants.java
@@ -9,4 +9,6 @@ public final class Constants {
   public static final String CPF_REGEX = "^\\d{11}$";
   public static final String TEL_REGEX = "^\\d{10}$";
   public static final String MONEY_REGEX = "^[0-9]+\\.[0-9]{2}$";
+  public static final String TX_MED_ENCARGOS = "^[0-9]{1}\\.[0-9]{2}$";
+  public static final String CNPJ_PPBANK = "09516419";
 }
